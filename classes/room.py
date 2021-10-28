ee2ed05c1dbf020a86cdebff621d58c7901a34a1
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:12:28 +0100

    adds check_in_guest, passes first test

--- classes/room.py
+++ classes/room.py
@@ -2,3 +2,6 @@ class Room:
     def __init__(self):
         self.guests = []
         self.songs = []
+
+    def check_in_guest(self, guest):
+        self.guests = [guest]
--- tests/room_test.py
+++ tests/room_test.py
@@ -8,7 +8,6 @@ class TestRoom(TestSetup):
     def test_room_hass_no_songs(self):
         self.assertEqual([], self.room1.songs)
 
-    @unittest.skip('')
     def test_check_in_guest(self):
         self.room1.check_in_guest(self.guest1)
         self.assertEqual(1, len(self.room1.guests))
