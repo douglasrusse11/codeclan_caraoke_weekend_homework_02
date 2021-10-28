4249e9e37b2ee5e926826adab3a4f481c91a40ba
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:09:35 +0100

    adds another guest in setUp

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -6,5 +6,6 @@ from classes.song import Song
 class TestSetup(unittest.TestCase):
     def setUp(self):
         self.guest1 = Guest("Barry")
+        self.guest2 = Guest("Belinda")
         self.room1 = Room()
         self.song1 = Song("Shoe Box Money", "Benny Sings")
