bf0d9cdda2b28a5197809d59194a350ac15338c3
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:03:20 +0100

    modifies __init__ for Song, passes second test

--- tests/song_test.py
+++ tests/song_test.py
@@ -5,6 +5,5 @@ class TestSong(TestSetup):
     def test_song_has_title(self):
         self.assertEqual("Shoe Box Money", self.song1.title)
     
-    @unittest.skip('')
     def test_song_has_artist(self):
         self.assertEqual("Benny Sings", self.song1.artist)
