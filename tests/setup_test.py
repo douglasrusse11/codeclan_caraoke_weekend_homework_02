47bb35a15b212a4c88f407a4ef769c5a5d123cf3
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 17:57:02 +0100

    adds import statements to setup_test

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -1,4 +1,7 @@
 import unittest
+from classes.guest import Guest
+from classes.room import Room
+from classes.song import Song
 
 class TestSetup(unittest.TestCase):
     def setUp(self):
--- tests/song_test.py
+++ tests/song_test.py
@@ -1,5 +1,4 @@
 import unittest
-from classes.song import Song
 from tests.setup_test import TestSetup
 
 class TestSong(TestSetup):
