c3a3e9184549186f14d3d3925d4fa1013821933a
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:19:35 +0100

    amends second test for check_out_guest

--- tests/room_test.py
+++ tests/room_test.py
@@ -22,11 +22,10 @@ class TestRoom(TestSetup):
         self.room1.check_out_guest(self.guest1)
         self.assertEqual([], self.room1.guests)
 
-    @unittest.skip('')
     def test_check_out_correct_guest(self):
         self.room1.check_in_guest(self.guest1)
         self.room1.check_in_guest(self.guest2)
         self.room1.check_out_guest(self.guest1)
-        self.assertEqual([], self.room1.guests)
+        self.assertEqual(1, len(self.room1.guests))
         self.assertTrue(self.guest2 in self.room1.guests)
         self.assertFalse(self.guest1 in self.room1.guests)
