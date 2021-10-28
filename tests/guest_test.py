349a2e5177fcb52133de82cf67c5db09a222537e
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 18:00:29 +0100

    modifies setUp for init tests

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,5 +1,4 @@
 import unittest
-from classes.guest import Guest
 from tests.setup_test import TestSetup
 
 class TestGuest(TestSetup):
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,5 +1,4 @@
 import unittest
-from classes.room import Room
 from tests.setup_test import TestSetup
 
 class TestRoom(TestSetup):
