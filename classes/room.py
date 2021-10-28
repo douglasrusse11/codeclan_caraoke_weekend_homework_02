8674d4c134cd212c2d9675f376e22f5ca8ada2cd
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:01:02 +0100

    modifies __init__ for Room, passes second test

--- classes/room.py
+++ classes/room.py
@@ -1,3 +1,4 @@
 class Room:
     def __init__(self):
         self.guests = []
+        self.songs = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -5,6 +5,5 @@ class TestRoom(TestSetup):
     def test_room_has_no_guests(self):
         self.assertEqual([], self.room1.guests)
 
-    @unittest.skip('')
     def test_room_hass_no_songs(self):
         self.assertEqual([], self.room1.songs)
