ac1ce3179f49065116eac102543f765d859b9193
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 18:57:34 +0100

    adds test skips

--- tests/room_test.py
+++ tests/room_test.py
@@ -2,8 +2,10 @@ import unittest
 from tests.setup_test import TestSetup
 
 class TestRoom(TestSetup):
+    @unittest.skip('')
     def test_room_has_no_guests(self):
         self.assertEqual([], self.room1.guests)
 
+    @unittest.skip('')
     def test_room_hass_no_songs(self):
         self.assertEqual([], self.room1.songs)
--- tests/song_test.py
+++ tests/song_test.py
@@ -2,8 +2,10 @@ import unittest
 from tests.setup_test import TestSetup
 
 class TestSong(TestSetup):
+    @unittest.skip('')
     def test_song_has_title(self):
         self.assertEqual("Shoe Box Money", self.song1.title)
     
+    @unittest.skip('')
     def test_song_has_artist(self):
         self.assertEqual("Benny Sings", self.song1.artist)
