ec279c723f772b2a946fa006ab83d47541cfbdf7
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 17:55:29 +0100

    adds init tests for Room

--- tests/room_test.py
+++ tests/room_test.py
@@ -3,4 +3,8 @@ from classes.room import Room
 from tests.setup_test import TestSetup
 
 class TestRoom(TestSetup):
-    pass
+    def test_room_has_no_guests(self):
+        self.assertEqual([], self.room1.guests)
+
+    def test_room_hass_no_songs(self):
+        self.assertEqual([], self.room1.songs)
