60930cc65fcba133934b48bf9188c24b2abb991a
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:08:41 +0100

    adds tests for check_in_guest

--- tests/room_test.py
+++ tests/room_test.py
@@ -7,3 +7,14 @@ class TestRoom(TestSetup):
 
     def test_room_hass_no_songs(self):
         self.assertEqual([], self.room1.songs)
+
+    @unittest.skip('')
+    def test_check_in_guest(self):
+        self.room1.check_in_guest(self.guest1)
+        self.assertEqual(1, len(self.room1.guests))
+
+    @unittest.skip('')
+    def test_check_in_multiple_guests(self):
+        self.room1.check_in_guest(self.guest1)
+        self.room1.check_in_guest(self.guest2)
+        self.assertEqual(2, len(self.room1.guests))
