39d7280273b3589630f64e74f2fe605ef260df90
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 17:50:25 +0100

    adds init test for Guest

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,4 +3,5 @@ from classes.guest import Guest
 from tests.setup_test import TestSetup
 
 class TestGuest(TestSetup):
-    pass
+    def test_guest_has_name(self):
+        self.assertEqual("Barry", self.guest1.name)
