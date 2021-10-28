6f9b4d093be888372292d1114264a2e911bed9f5
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-28 19:17:26 +0100

    adds tests for check_out_guest

--- tests/room_test.py
+++ tests/room_test.py
@@ -16,3 +16,18 @@ class TestRoom(TestSetup):
         self.room1.check_in_guest(self.guest1)
         self.room1.check_in_guest(self.guest2)
         self.assertEqual(2, len(self.room1.guests))
+
+    @unittest.skip('')
+    def test_check_out_guest(self):
+        self.room1.check_in_guest(self.guest1)
+        self.room1.check_out_guest(self.guest1)
+        self.assertEqual([], self.room1.guests)
+
+    @unittest.skip('')
+    def test_check_out_correct_guest(self):
+        self.room1.check_in_guest(self.guest1)
+        self.room1.check_in_guest(self.guest2)
+        self.room1.check_out_guest(self.guest1)
+        self.assertEqual([], self.room1.guests)
+        self.assertTrue(self.guest2 in self.room1.guests)
+        self.assertFalse(self.guest1 in self.room1.guests)
