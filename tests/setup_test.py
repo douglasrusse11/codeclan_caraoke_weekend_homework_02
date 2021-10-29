1bf84a6b6a2ac82806ebdc2e4f71047232249dec
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 11:40:15 +0100

    adds integration tests for generate_playlist

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -12,6 +12,7 @@ class TestSetup(unittest.TestCase):
         self.guest2 = Guest("Belinda", 15, self.song2)
         self.guest3 = Guest("Bill", 2, self.song2)
         self.guest4 = Guest("Bobette", 7, self.song1)
+        self.guest5 = Guest("Buck", 12, self.song3)
         self.room1 = Room(2, 5)
-        self.room2 = Room(5, 5)
+        self.room2 = Room(5, 2)
         
