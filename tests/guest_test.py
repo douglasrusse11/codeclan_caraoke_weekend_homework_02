209679916b1efbadcf08f78d2959474e64516a80
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 07:17:10 +0100

    adds wallet argument to Guest in setUp

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,6 +5,5 @@ class TestGuest(TestSetup):
     def test_guest_has_name(self):
         self.assertEqual("Barry", self.guest1.name)
 
-    @unittest.skip('')
     def test_guest_wallet(self):
         self.assertEqual(10, self.guest1.wallet)
--- tests/setup_test.py
+++ tests/setup_test.py
@@ -5,8 +5,8 @@ from classes.song import Song
 
 class TestSetup(unittest.TestCase):
     def setUp(self):
-        self.guest1 = Guest("Barry")
-        self.guest2 = Guest("Belinda")
+        self.guest1 = Guest("Barry", 10)
+        self.guest2 = Guest("Belinda", 15)
         self.room1 = Room()
         self.song1 = Song("Shoe Box Money", "Benny Sings")
         self.song2 = Song("Baleine", "Purl")
