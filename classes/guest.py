18ee165c95fee6234ceb1ce4ec421ac3f72e9549
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 08:52:11 +0100

    modifies __init__ for Guest

--- classes/guest.py
+++ classes/guest.py
@@ -1,7 +1,8 @@
 class Guest:
-    def __init__(self, name, wallet):
+    def __init__(self, name, wallet, favourite_song):
         self.name = name
         self.wallet = wallet
+        self.favourite_song = favourite_song
 
     def can_afford_entry_fee(self, fee):
         return self.wallet >= fee
