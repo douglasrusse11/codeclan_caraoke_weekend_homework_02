93c96c141f99c1fff6c2e105a89dd61c7e1714da
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 08:49:25 +0100

    modifies setUp

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -5,10 +5,11 @@ from classes.song import Song
 
 class TestSetup(unittest.TestCase):
     def setUp(self):
-        self.guest1 = Guest("Barry", 10)
-        self.guest2 = Guest("Belinda", 15)
-        self.guest3 = Guest("Bill", 2)
-        self.guest4 = Guest("Bobette", 7)
-        self.room1 = Room(2, 5)
         self.song1 = Song("Shoe Box Money", "Benny Sings")
-        self.song2 = Song("Baleine", "Purl")
+        self.song2 = Song("Bounding", "Ice Choir")
+        self.guest1 = Guest("Barry", 10, self.song1)
+        self.guest2 = Guest("Belinda", 15, self.song2)
+        self.guest3 = Guest("Bill", 2, self.song2)
+        self.guest4 = Guest("Bobette", 7, self.song1)
+        self.room1 = Room(2, 5)
+        
