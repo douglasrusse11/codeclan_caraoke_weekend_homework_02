c417f58798c26951103a2c1ed1e37db4e8018a7b
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 07:19:32 +0100

    adds capacity and fee arguments for Room in setUp

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -7,6 +7,6 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         self.guest1 = Guest("Barry", 10)
         self.guest2 = Guest("Belinda", 15)
-        self.room1 = Room()
+        self.room1 = Room(2, 5)
         self.song1 = Song("Shoe Box Money", "Benny Sings")
         self.song2 = Song("Baleine", "Purl")
