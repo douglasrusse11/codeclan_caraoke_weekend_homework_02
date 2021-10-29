9456edbdad7be3fae6de96cadb930dc03f174f8d
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-29 08:59:33 +0100

    adds tests for cheer

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -18,3 +18,13 @@ class TestGuest(TestSetup):
     def test_guest_cannot_afford_entry_fee(self):
         self.fee = 15
         self.assertFalse(self.guest1.can_afford_entry_fee(self.fee))
+
+    @unittest.skip('')
+    def test_guest_cheer(self)
+        songs = [self.song1, self.song2]
+        self.assertEqual("Whoo!", self.guest1.cheer(songs))
+
+    @unittest.skip('')
+    def test_guest_cheer_song_not_in_room(self):
+        songs =[self.song1]
+        self.assertEqual(None, self.guest2.cheer(songs))
